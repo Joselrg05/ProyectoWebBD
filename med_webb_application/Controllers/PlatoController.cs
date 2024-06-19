a7be94fc91c9009abd5304a5a77662f4afbcb7ce
José Rodrígueez <jr7035960@gmail.com>
2024-06-19 12:57:50 -0600

    Implementación de reporting services

---
+++ med_webb_application/Controllers/PlatoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace med_webb_application.Controllers
+{
+    public class PlatoController : Controller
+    {
+        // GET: Plato
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
